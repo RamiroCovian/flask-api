d6f4571972d0e454a2f030ff5130bd5664bc87d8
RamiroCovian <ramiro.covian@gmail.com>
2023-10-10 11:02:13 -0300

    feat: creamos la primera vista y plantilla html

---
+++ balance/api.py
@@ -0,0 +1,24 @@
+from flask import jsonify
+
+from . import app
+from .models import DBManager
+
+
+############## VISTAS API
+@app.route("/api/v1/movimientos")
+def listar_movimientos():
+    try:
+        # db = DBManager(app.config.get("RUTA"))
+        """
+        Hace excepcion para la configuracion tomar el dato en vez de con GET,
+        se haga de esta manera porque si la ruta no esta configurada, de error,
+        de la otra manera(con GET), sigue funcionando el programa
+        """
+        db = DBManager(app.config["RUTA"])
+
+        sql = "SELECT fecha, concepto, tipo, cantidad FROM movimientos"
+        movs = db.consultaSQL(sql)
+        resultado = {"status": "success", "results": movs}
+    except Exception as ex:
+        resultado = {"message": str(ex), "status": "error"}
+    return jsonify(resultado)
--- balance/views.py
+++ balance/views.py
@@ -1,23 +1,10 @@
-from flask import jsonify
+from flask import render_template
 
 from . import app
-from .models import DBManager
+
+############## VISTAS PARA TEMPLATES
 
 
 @app.route("/")
-def inicio():
-    try:
-        # db = DBManager(app.config.get("RUTA"))
-        """
-        Hace excepcion para la configuracion tomar el dato en vez de con GET,
-        se haga de esta manera porque si la ruta no esta configurada, de error,
-        de la otra manera(con GET), sigue funcionando el programa
-        """
-        db = DBManager(app.config["RUTA"])
-
-        sql = "SELECT fecha, concepto, tipo, cantidad FROM movimientos"
-        movs = db.consultaSQL(sql)
-        resultado = {"status": "success", "results": movs}
-    except Exception as ex:
-        resultado = {"message": str(ex), "status": "error"}
-    return jsonify(resultado)
+def home():
+    return render_template("inicio.html")
