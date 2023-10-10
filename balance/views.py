12700b5766c749beb7e112f2942305ac1ae6a152
RamiroCovian <ramiro.covian@gmail.com>
2023-10-10 10:53:41 -0300

    feat: mejoramos la respuesta JSON

--- balance/views.py
+++ balance/views.py
@@ -6,14 +6,18 @@ from .models import DBManager
 
 @app.route("/")
 def inicio():
-    # db = DBManager(app.config.get("RUTA"))
-    """
-    Hace excepcion para la configuracion tomar el dato en vez de con GET,
-    se haga de esta manera porque si la ruta no esta configurada, de error,
-    de la otra manera(con GET), sigue funcionando el programa
-    """
-    db = DBManager(app.config["RUTA"])
+    try:
+        # db = DBManager(app.config.get("RUTA"))
+        """
+        Hace excepcion para la configuracion tomar el dato en vez de con GET,
+        se haga de esta manera porque si la ruta no esta configurada, de error,
+        de la otra manera(con GET), sigue funcionando el programa
+        """
+        db = DBManager(app.config["RUTA"])
 
-    sql = "SELECT fecha, concepto, tipo, cantidad FROM movimientos"
-    movs = db.consultaSQL(sql)
-    return jsonify(movs)
+        sql = "SELECT fecha, concepto, tipo, cantidad FROM movimientos"
+        movs = db.consultaSQL(sql)
+        resultado = {"status": "success", "results": movs}
+    except Exception as ex:
+        resultado = {"message": str(ex), "status": "error"}
+    return jsonify(resultado)
