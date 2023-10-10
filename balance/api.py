ecd4ccdfb0f41e099071e81c909670b3239ca79b
RamiroCovian <ramiro.covian@gmail.com>
2023-10-10 16:48:47 -0300

    feat: endpoint para cargar un movimiento

--- balance/api.py
+++ balance/api.py
@@ -22,3 +22,35 @@ def listar_movimientos():
     except Exception as ex:
         resultado = {"message": str(ex), "status": "error"}
     return jsonify(resultado)
+
+
+@app.route("/api/v1/movimientos/<int:id>")
+def leer_movimiento(id):
+    """
+    1. Instanciar DBManager
+    2. Preparar la consulta
+    3. Ejecutar la consulta
+    4. Leer el resultado
+    5. Si es OK:
+    6.   status success y movimiento
+    7. Si es KO:
+    8.   status error y mensaje
+    9. Devolver resultado (movimiento)
+    """
+    try:
+        db = DBManager(app.config["RUTA"])
+        mov = db.obtenerMovimiento(id)
+        if mov:
+            resultado = {"status": "success", "results": mov}
+            status_code = 200
+        else:
+            resultado = {
+                "status": "error",
+                "message": f"No he encontrado el movimiento con ID = {id}",
+            }
+            status_code = 404
+
+    except Exception as ex:
+        resultado = {"status": "error", "message": str(ex)}
+        status_code = 500
+    return jsonify(resultado), status_code
