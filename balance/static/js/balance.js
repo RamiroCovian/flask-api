1701966c4176775c765ec16a1d5494efb03e9661
RamiroCovian <ramiro.covian@gmail.com>
2023-10-10 15:40:30 -0300

    feat: pintamos los movimientos en la tabla

--- balance/static/js/balance.js
+++ balance/static/js/balance.js
@@ -4,9 +4,29 @@ const peticion = new XMLHttpRequest();
 // alert("Hola desde JavaScript");
 function cargarMovimientos() {
     console.log("Has llamado a la funcion de cargar movimientos");
-    peticion.open("GET", "http://localhost:5000/api/v1/movimientos", false);
+    peticion.open("GET", "http://127.0.0.1:5000/api/v1/movimientos", false);
     peticion.send();
-    console.log(peticion.responseText);
+    const resultado = JSON.parse(peticion.responseText);
+    const movimientos = resultado.results;
+
+    let html = "";
+    // En Python:
+    // for i in Range(len(movimientos)):
+    //     i = i + 1
+    for (let i = 0; i < movimientos.length; i = i + 1) {
+        const mov = movimientos[i]
+        html = html + `
+            <tr>
+                <td>${mov.fecha}</td> 
+                <td>${mov.concepto}</td>
+                <td>${mov.tipo}</td>
+                <td>${mov.cantidad}</td>
+            </tr>
+        `;
+    }
+    console.log("html", html);
+    const tabla = document.querySelector("#cuerpo-tabla");
+    tabla.innerHTML = html;
 }
 
 window.onload = function () {
